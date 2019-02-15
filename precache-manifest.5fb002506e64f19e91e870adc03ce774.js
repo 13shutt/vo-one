9d7f7d48bb65c4a48137ccc449aa2cad813663bb
13shutt <shutt13a@gmail.com>
2019-02-15 20:29:31 +0200

    Updates

---
+++ precache-manifest.5fb002506e64f19e91e870adc03ce774.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "e603c690f754c70654004a753c17b60b",
+    "url": "/static/media/HelveticaNeueCyr-Medium.e603c690.ttf"
+  },
+  {
+    "revision": "307781839e3e303ff49b223dc242baac",
+    "url": "/static/media/HelveticaNeueCyr-Light.30778183.ttf"
+  },
+  {
+    "revision": "303f6a538da65cd9cd71f5cdd0d92125",
+    "url": "/static/media/HelveticaNeueCyr-Black.303f6a53.ttf"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "1a380d13a552b9a4d03d",
+    "url": "/static/js/main.1a380d13.chunk.js"
+  },
+  {
+    "revision": "e6607eb32e6998d294df",
+    "url": "/static/js/2.e6607eb3.chunk.js"
+  },
+  {
+    "revision": "79b85abe3b31aac640781a3dde982a8f",
+    "url": "/index.html"
+  }
+];
