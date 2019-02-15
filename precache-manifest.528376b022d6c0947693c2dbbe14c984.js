b40ec19d4085a513b7099043851fe62f7f8d4a0f
13shutt <shutt13a@gmail.com>
2019-02-15 21:00:51 +0200

    Updates

---
+++ precache-manifest.528376b022d6c0947693c2dbbe14c984.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "e603c690f754c70654004a753c17b60b",
+    "url": "https://13shutt.github.io/static/media/HelveticaNeueCyr-Medium.e603c690.ttf"
+  },
+  {
+    "revision": "307781839e3e303ff49b223dc242baac",
+    "url": "https://13shutt.github.io/static/media/HelveticaNeueCyr-Light.30778183.ttf"
+  },
+  {
+    "revision": "303f6a538da65cd9cd71f5cdd0d92125",
+    "url": "https://13shutt.github.io/static/media/HelveticaNeueCyr-Black.303f6a53.ttf"
+  },
+  {
+    "revision": "99ce4433784c1b945a13",
+    "url": "https://13shutt.github.io/static/js/runtime~main.99ce4433.js"
+  },
+  {
+    "revision": "1a380d13a552b9a4d03d",
+    "url": "https://13shutt.github.io/static/js/main.1a380d13.chunk.js"
+  },
+  {
+    "revision": "3e2e15b7fe6a251b5d56",
+    "url": "https://13shutt.github.io/static/js/2.3e2e15b7.chunk.js"
+  },
+  {
+    "revision": "81dc8155ef96964d1d76e9e63de9de8d",
+    "url": "https://13shutt.github.io/index.html"
+  }
+];
