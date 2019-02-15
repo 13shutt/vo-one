cbf4e65d861601a33c33cefc67a92ebb0fbbb5af
13shutt <shutt13a@gmail.com>
2019-02-15 20:57:54 +0200

    Updates

---
+++ precache-manifest.012ac18cfc82b18670338264bc7fa7e8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "e603c690f754c70654004a753c17b60b",
+    "url": "/vo-one/static/media/HelveticaNeueCyr-Medium.e603c690.ttf"
+  },
+  {
+    "revision": "307781839e3e303ff49b223dc242baac",
+    "url": "/vo-one/static/media/HelveticaNeueCyr-Light.30778183.ttf"
+  },
+  {
+    "revision": "303f6a538da65cd9cd71f5cdd0d92125",
+    "url": "/vo-one/static/media/HelveticaNeueCyr-Black.303f6a53.ttf"
+  },
+  {
+    "revision": "5d8950894108c74e5374",
+    "url": "/vo-one/static/js/runtime~main.5d895089.js"
+  },
+  {
+    "revision": "1a380d13a552b9a4d03d",
+    "url": "/vo-one/static/js/main.1a380d13.chunk.js"
+  },
+  {
+    "revision": "e8ba2458f24d550b441c",
+    "url": "/vo-one/static/js/2.e8ba2458.chunk.js"
+  },
+  {
+    "revision": "2b0741345fce8e94fb3a43a5190362fd",
+    "url": "/vo-one/index.html"
+  }
+];
